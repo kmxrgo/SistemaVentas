daad8381b1abb10660a3af1dc7ab666b045968d1
ValentinPacheco <valepachecoolivas@gmail.com>
2026-02-19 00:49:53 -0700

    se cambio el program para que inicie en el frm listado categoria

--- SistemaVentas/Program.cs
+++ SistemaVentas/Program.cs
@@ -16,7 +16,7 @@ namespace SistemaVentas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CapaPresentacion.FrmListadoCliente());
+            Application.Run(new CapaPresentacion.FrmListadoCategoria());
         }
     }
 }
