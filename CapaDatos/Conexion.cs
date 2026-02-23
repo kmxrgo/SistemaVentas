3b08226aae9a2a305b6bf57c9b761999717d5410
ValentinPacheco <valepachecoolivas@gmail.com>
2026-02-22 22:15:15 -0700

    cambien la conexion y ya

--- CapaDatos/Conexion.cs
+++ CapaDatos/Conexion.cs
@@ -8,6 +8,6 @@ namespace CapaDatos
 {
     public class Conexion
     {   
-        public static string Conn = "Data source = CAMARGOLAPTOP; Initial Catalog = dbventas; Integrated Security = true";
+        public static string Conn = "Data source = ValeLaptop; Initial Catalog = dbventas; Integrated Security = true";
     }
 }
